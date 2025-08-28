03d2790d1a1171110a5fd8490f8261c7e07c2df1
iysaofficial <youngscientist.iysa@gmail.com>
2025-08-28 11:54:16 +0700

    regis

--- src/components/registration/HomeRegistrationComp.tsx
+++ src/components/registration/HomeRegistrationComp.tsx
@@ -3,14 +3,14 @@ import Link from "next/link"
 const HomeRegistrationComp = () =>{
     return(
         <>
-        <section className="homeregist-section">
-            <div className="wrapper">
+        <section className="">
+            <div className="">
             <div className="">
                 <h2 className="">REGISTRATION FORM</h2>
                 <h3 className="">Choose Categories Participant for Registration IYBO 2026</h3>
             </div>
             </div>
-            <div className="wrapper">
+            <div className="">
                 <Link href="/registration/international" className="">International Participant</Link>
                 <Link href="/registration/national" className="">Indonesian Participant</Link>
             </div>
